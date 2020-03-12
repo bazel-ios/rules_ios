b6c871f6d3a925085bb86f8a19b6a42c89d3eda8
Amber Dixon <amberdixon@squareup.com>
2020-03-12 15:08:36 -0700

    For iOS applications, these are the destination paths under derived data for swift modules

--- tools/xcodeproj-shims/installers/_swiftmodule.sh
+++ tools/xcodeproj-shims/installers/_swiftmodule.sh
@@ -7,7 +7,13 @@ for module in "$@"; do
     module_name=$(basename "$module")
     module_bundle="$BUILT_PRODUCTS_DIR/$module_name"
     mkdir -p "$module_bundle"
-    cp "$module" "$module_bundle/$CURRENT_ARCH.swiftmodule"
-    cp "$doc" "$module_bundle/$CURRENT_ARCH.swiftdoc"
+    # TODO: understand the correct logic here
+    cp "$module" "$module_bundle/$NATIVE_ARCH_ACTUAL.swiftmodule"
+    cp "$doc" "$module_bundle/$NATIVE_ARCH_ACTUAL.swiftdoc"
+
+    cp "$module" "$module_bundle/$NATIVE_ARCH_ACTUAL-$LLVM_TARGET_TRIPLE_VENDOR-$SWIFT_PLATFORM_TARGET_PREFIX$LLVM_TARGET_TRIPLE_SUFFIX.swiftmodule"
+    cp "$doc" "$module_bundle/$NATIVE_ARCH_ACTUAL-$LLVM_TARGET_TRIPLE_VENDOR-$SWIFT_PLATFORM_TARGET_PREFIX$LLVM_TARGET_TRIPLE_SUFFIX.swiftdoc"
+
+    chmod -R +w "$module_bundle"
     chmod -R +w "$module_bundle"
 done
