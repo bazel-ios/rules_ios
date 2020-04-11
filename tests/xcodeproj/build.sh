9f671c3187b84e598cfd7a83fa805cbc50e3b401
Amber Dixon <amberdixon@squareup.com>
2020-04-10 21:07:40 -0700

    build and test on the same destination

--- tests/xcodeproj/build.sh
+++ tests/xcodeproj/build.sh
@@ -3,7 +3,8 @@ set -eux
 cd $(dirname $0)
 
 # xcodebuild -project Single-Application-Project.xcodeproj -quiet
-xcodebuild -project Single-Application-Project.xcodeproj
-ls -R1 /Users/runner/Library/Developer/Xcode/DerivedData/Single-Application-Project-*
 export SIM_DEVICE_ID=`xcodebuild -project Single-Application-Project.xcodeproj -scheme "Single-Application-UnitTests" -showdestinations | grep "platform:iOS Sim" | head -1 | ruby -e "puts STDIN.read.split(',')[1].split(':').last"`
+xcodebuild -project Single-Application-Project.xcodeproj -destination "id=$SIM_DEVICE_ID"
+ls -R1 /Users/runner/Library/Developer/Xcode/DerivedData/Single-Application-Project-*
 xcodebuild -project Single-Application-Project.xcodeproj -scheme "Single-Application-UnitTests"  test-without-building -destination "id=$SIM_DEVICE_ID"
+
