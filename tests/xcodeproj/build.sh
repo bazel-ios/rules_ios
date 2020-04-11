0d88b90c0b63d39694ee988e950baf6266643bf7
Amber Dixon <amberdixon@squareup.com>
2020-04-10 20:50:05 -0700

    more debugging for CI test failures

--- tests/xcodeproj/build.sh
+++ tests/xcodeproj/build.sh
@@ -2,6 +2,8 @@ set -eux
 
 cd $(dirname $0)
 
-xcodebuild -project Single-Application-Project.xcodeproj -quiet
+# xcodebuild -project Single-Application-Project.xcodeproj -quiet
+xcodebuild -project Single-Application-Project.xcodeproj
+ls -R1 /Users/runner/Library/Developer/Xcode/DerivedData/Single-Application-Project-*
 export SIM_DEVICE_ID=`xcodebuild -project Single-Application-Project.xcodeproj -scheme "Single-Application-UnitTests" -showdestinations | grep "platform:iOS Sim" | head -1 | ruby -e "puts STDIN.read.split(',')[1].split(':').last"`
 xcodebuild -project Single-Application-Project.xcodeproj -scheme "Single-Application-UnitTests"  test-without-building -destination "id=$SIM_DEVICE_ID"
