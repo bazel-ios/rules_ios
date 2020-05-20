85bd4d52c46bf4dcc9a7119da0cbd5aaf31562c8
Amber Dixon <amberdixon@squareup.com>
2020-05-19 21:59:36 -0700

    Add indexing support

---
+++ tools/xcodeproj_shims/installers/indexstores.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# See `_indexstore.sh` for full details.
+
+# Make sure add these to bazel build target copts for objective-c
+find bazel-out/*/bin/ \
+     -type d \
+     -name "*.indexstore" \
+     -print0 \
+    | xargs -0 "$BAZEL_INSTALLERS_DIR/_indexstore.sh"
+
+echo "Finish remapping index files"
