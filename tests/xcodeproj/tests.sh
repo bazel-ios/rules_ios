241b76eba8e3501ce5b4d6a32158fda37d02291d
Amber Dixon <amberdixon@squareup.com>
2020-05-20 21:00:21 -0700

    so this works

--- tests/xcodeproj/tests.sh
+++ tests/xcodeproj/tests.sh
@@ -2,10 +2,12 @@ set -eux
 
 cd $(dirname $0)
 
-xcodebuild -project Single-Application-Project-AllTargets.xcodeproj -quiet -scheme Single-Application-UnitTests test
-xcodebuild -project Single-Application-Project-AllTargets.xcodeproj -quiet -scheme Single-Application-RunnableTestSuite test
+export SIM_DEVICE_ID=`xcodebuild -project Single-Application-Project-AllTargets.xcodeproj -scheme Single-Application-UnitTests -showdestinations | grep "platform:iOS Sim" | head -1 | ruby -e "puts STDIN.read.split(',')[1].split(':').last"`
+xcodebuild -project Single-Application-Project-AllTargets.xcodeproj -quiet -scheme Single-Application-UnitTests -destination "id=$SIM_DEVICE_ID" test
+xcodebuild -project Single-Application-Project-AllTargets.xcodeproj -quiet -scheme Single-Application-RunnableTestSuite -destination "id=$SIM_DEVICE_ID" test
 
-xcodebuild -project Single-Application-Project-DirectTargetsOnly.xcodeproj -quiet -scheme Single-Application-UnitTests test
-xcodebuild -project Single-Application-Project-DirectTargetsOnly.xcodeproj -quiet -scheme Single-Application-RunnableTestSuite test
+xcodebuild -project Single-Application-Project-DirectTargetsOnly.xcodeproj -quiet -scheme Single-Application-UnitTests -destination "id=$SIM_DEVICE_ID" test
+xcodebuild -project Single-Application-Project-DirectTargetsOnly.xcodeproj -quiet -scheme Single-Application-RunnableTestSuite -destination "id=$SIM_DEVICE_ID" test
 
-xcodebuild -project Test-Target-With-Test-Host-Project.xcodeproj -quiet -scheme TestImports-Unit-Tests test
+xcodebuild -project Test-Target-With-Test-Host-Project.xcodeproj -quiet -scheme TestImports-Unit-Tests -destination "id=$SIM_DEVICE_ID" test
+
