adb76a7eae5051097a559d9506949de746213163
Amber Dixon <amberdixon@squareup.com>
2020-08-03 11:17:52 -0700

    Address build failures, trailing newlines

--- tests/macos/xcodeproj/Single-Application-Project-DirectTargetsOnly.xcodeproj/bazelinstallers/swiftmodules.sh
+++ tests/macos/xcodeproj/Single-Application-Project-DirectTargetsOnly.xcodeproj/bazelinstallers/swiftmodules.sh
@@ -4,8 +4,21 @@ set -euo pipefail
 
 # Copy Bazel build `.swiftmodule` files to `DerivedData`. This is used by Xcode
 # and its indexing.
-find "$BAZEL_WORKSPACE_ROOT/bazel-out"/*/bin/ \
-    -name "*.swiftmodule" \
-     -not -path "*/_swift_module_cache/*" \
-     -print0 \
-    | xargs -0 "$BAZEL_INSTALLERS_DIR/_swiftmodule.sh"
+echo "Start copying swiftmodules at `date`"
+FOUND_SWIFTMODULES=`pcregrep -o1 'primary_output: "(.*\.swiftmodule)' $BAZEL_BUILD_EVENT_TEXT_FILENAME | uniq`
+declare -a EXISTING_SWIFTMODULES=()
+for i in $FOUND_SWIFTMODULES
+do
+  if [[ -f $i ]]
+  then
+    EXISTING_SWIFTMODULES+=($i)
+  fi
+done
+echo "Found ${#EXISTING_SWIFTMODULES[@]} existing SWIFTMODULES"
+if [ ${#EXISTING_SWIFTMODULES[@]} -ne 0 ]
+then
+  "$BAZEL_INSTALLERS_DIR/_swiftmodule.sh" ${EXISTING_SWIFTMODULES[*]}
+else
+  echo "No SWIFTMODULES found"
+fi
+echo "Finish copying swiftmodules at `date`"
