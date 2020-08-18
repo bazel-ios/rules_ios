31d0718d6b3b5857c4426c137e88443a1185cfeb
Yuanfeng Gao <yuanfeng@squareup.com>
2020-08-18 18:25:07 -0400

    indent the script so it makes more sense

--- tools/xcodeproj_shims/build-wrapper.sh
+++ tools/xcodeproj_shims/build-wrapper.sh
@@ -2,9 +2,9 @@
 set -euxo pipefail
 
 $BAZEL_PATH build \
---experimental_execution_log_file=$BAZEL_BUILD_EXECUTION_LOG_FILENAME \
---build_event_text_file=$BAZEL_BUILD_EVENT_TEXT_FILENAME \
---build_event_publish_all_actions $1 \
-$BAZEL_RULES_IOS_OPTIONS \
-2>&1 \
-| $BAZEL_OUTPUT_PROCESSOR
+    --experimental_execution_log_file=$BAZEL_BUILD_EXECUTION_LOG_FILENAME \
+    --build_event_text_file=$BAZEL_BUILD_EVENT_TEXT_FILENAME \
+    --build_event_publish_all_actions $1 \
+    $BAZEL_RULES_IOS_OPTIONS \
+    2>&1 \
+    | $BAZEL_OUTPUT_PROCESSOR
