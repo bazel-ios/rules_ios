7cc8d1df22f0f1a7862ca0bf5358238e146628fe
Amber Dixon <amberdixon@squareup.com>
2020-08-19 15:12:01 -0700

    add back test for header search paths

--- tests/ios/xcodeproj/post_build_check.sh
+++ tests/ios/xcodeproj/post_build_check.sh
@@ -2,6 +2,16 @@ set -eux
 
 cd $(dirname $0)
 
+export HSP=`grep "HEADER_SEARCH_PATHS" *.xcodeproj/project.pbxproj | grep -o  "bazel-out\S*\.hmap"`
+echo "Make sure hmap files exist after build"
+for path in ${HSP}; do
+    FULL_PATH="../../../$path"
+    if [ ! -f $FULL_PATH ]; then
+        echo "File at $FULL_PATH not found!";
+        exit 1;
+    fi
+done
+
 export FSP=`grep "FRAMEWORK_SEARCH_PATHS" *.xcodeproj/project.pbxproj | grep -o "bazel-out[^ \\]*"`
 echo "Make sure framework search paths exist after build"
 for path in ${FSP}; do
