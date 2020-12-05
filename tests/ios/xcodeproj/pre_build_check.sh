57cc1e00d981f3a790ba33cfda9f53f37262c5a8
Samuel Giddins <segiddins@squareup.com>
2020-12-04 17:06:26 -0800

    rm -f hmaps in tests, since they can be read-only

--- tests/ios/xcodeproj/pre_build_check.sh
+++ tests/ios/xcodeproj/pre_build_check.sh
@@ -2,23 +2,23 @@ set -eu
 
 cd $(dirname $0)
 
-export HSP=`grep "HEADER_SEARCH_PATHS" *.xcodeproj/project.pbxproj | grep -o  "bazel-out\S*\.hmap"`
+export HSP=$(grep "HEADER_SEARCH_PATHS" *.xcodeproj/project.pbxproj | grep -o "bazel-out\S*\.hmap")
 echo "Ensure hmap files do not exist (remove if they do)"
 for path in ${HSP}; do
     FULL_PATH="../../../$path"
     if [ -f $FULL_PATH ]; then
-        rm $FULL_PATH;
-        echo "Removed file at $FULL_PATH";
+        rm -f $FULL_PATH
+        echo "Removed file at $FULL_PATH"
     fi
 done
 
-export FSP=`grep "FRAMEWORK_SEARCH_PATHS" *.xcodeproj/project.pbxproj | grep -o "bazel-out[^ \\]*"`
+export FSP=$(grep "FRAMEWORK_SEARCH_PATHS" *.xcodeproj/project.pbxproj | grep -o "bazel-out[^ \]*")
 echo "Ensure framework search paths do not exist"
 for path in ${FSP}; do
     FULL_PATH="../../../$path"
     if [ -f $FULL_PATH ]; then
-        rm -rf $FULL_PATH;
-        echo "Removed file at $FULL_PATH";
+        rm -rf $FULL_PATH
+        echo "Removed file at $FULL_PATH"
     fi
 done
 
