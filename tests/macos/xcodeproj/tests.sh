6b8a9829db2836b7035df7fc5dca7bd59cf925b1
Samuel Giddins <segiddins@squareup.com>
2020-12-04 17:08:05 -0800

    Specify we’re building for ios-simulator when building test projects

--- tests/macos/xcodeproj/tests.sh
+++ tests/macos/xcodeproj/tests.sh
@@ -5,12 +5,12 @@ cd $(dirname $0)
 # This implicitly creates the simulators for use if not exist
 xcrun simctl list
 
-export SIM_DEVICE_ID=`xcodebuild -project Single-Application-Project-AllTargets.xcodeproj -scheme Single-Application-UnitTests -showdestinations | grep "platform:iOS Sim" | head -1 | ruby -e "puts STDIN.read.split(',')[1].split(':').last"`
-xcodebuild -project Single-Application-Project-AllTargets.xcodeproj -quiet -scheme Single-Application-UnitTests -destination "id=$SIM_DEVICE_ID" test
-xcodebuild -project Single-Application-Project-AllTargets.xcodeproj -quiet -scheme Single-Application-RunnableTestSuite -destination "id=$SIM_DEVICE_ID" test
+export SIM_DEVICE_ID=$(xcodebuild -project Single-Application-Project-AllTargets.xcodeproj -scheme Single-Application-UnitTests -showdestinations -destination "generic/platform=iOS Simulator" | grep "platform:iOS Sim" | head -1 | ruby -e "puts STDIN.read.split(',')[1].split(':').last")
 
-xcodebuild -project Single-Application-Project-DirectTargetsOnly.xcodeproj -quiet -scheme Single-Application-UnitTests -destination "id=$SIM_DEVICE_ID" test
-xcodebuild -project Single-Application-Project-DirectTargetsOnly.xcodeproj -quiet -scheme Single-Application-RunnableTestSuite -destination "id=$SIM_DEVICE_ID" test
+xcodebuild -project Single-Application-Project-AllTargets.xcodeproj -quiet -scheme Single-Application-UnitTests -destination "platform=iOS Simulator,id=$SIM_DEVICE_ID" test
+xcodebuild -project Single-Application-Project-AllTargets.xcodeproj -quiet -scheme Single-Application-RunnableTestSuite -destination "platform=iOS Simulator,id=$SIM_DEVICE_ID" test
 
-xcodebuild -project Test-Target-With-Test-Host-Project.xcodeproj -quiet -scheme TestImports-Unit-Tests -destination "id=$SIM_DEVICE_ID" test
+xcodebuild -project Single-Application-Project-DirectTargetsOnly.xcodeproj -quiet -scheme Single-Application-UnitTests -destination "platform=iOS Simulator,id=$SIM_DEVICE_ID" test
+xcodebuild -project Single-Application-Project-DirectTargetsOnly.xcodeproj -quiet -scheme Single-Application-RunnableTestSuite -destination "platform=iOS Simulator,id=$SIM_DEVICE_ID" test
 
+xcodebuild -project Test-Target-With-Test-Host-Project.xcodeproj -quiet -scheme TestImports-Unit-Tests -destination "platform=iOS Simulator,id=$SIM_DEVICE_ID" test
