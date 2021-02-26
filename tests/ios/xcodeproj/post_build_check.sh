b3316d1d0562d7e2870c706f1209c074b8725618
Thiago Cruz <thiago@squareup.com>
2021-02-26 10:28:09 -0800

    Post build check is not necessary anymore

--- tests/ios/xcodeproj/post_build_check.sh
+++ tests/ios/xcodeproj/post_build_check.sh
@@ -19,24 +19,11 @@ for path in ${HSP}; do
 done
 
 export FSP=$(grep -w "FRAMEWORK_SEARCH_PATHS" *.xcodeproj/project.pbxproj | grep -o "bazel-out[^ \]*")
-# 'cat' all generated files containing framework search paths
-# to be passed to LLDB
-#
-# This is used below to ensure all 'FRAMEWORK_SEARCH_PATHS'
-# are being passed to LLDB
-LLDB_FSP_FILES=$(grep "BAZEL_LLDB_FRAMEWORK_SEARCH_PATHS_FILE" *.xcodeproj/project.pbxproj | grep -o "bazel-out[^ \"\]*" | sed 's/^/..\/..\/..\//')
-export LLDB_FSP=$(cat $LLDB_FSP_FILES)
-
-echo "Make sure framework search paths exist after build and are being passed to LLDB"
+echo "Make sure framework search paths exist after build"
 for path in ${FSP}; do
     FULL_PATH="../../../$path"
     if [ ! -e $FULL_PATH ]; then
         echo "File at $FULL_PATH not found!"
-        exit 1
-    fi
-
-    if [[ ! "$LLDB_FSP" == *"$path"* ]]; then
-        echo "The path '$FULL_PATH' won't be passed to LLDB! The setting 'BAZEL_LLDB_SWIFT_EXTRA_CLANG_FLAGS' should contain this path."
         exit 1
     fi
 done
