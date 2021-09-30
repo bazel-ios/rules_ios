8e286697009c426e0c0c6e8e17800cd866e07bd4
i@jerrymarino.com <i@jerrymarino.com>
2021-09-30 12:29:34 -0700

    Disable extra header path

--- tests/ios/unit-test/test-imports-app/Header.h
+++ tests/ios/unit-test/test-imports-app/Header.h
@@ -2,4 +2,4 @@
 // - Xcode target name
 // - "Module" name
 #import <TestImports-App/Header2.h> // Path from headermap
-#import <TestImports_App/Header2.h> // Actual path
+// #import <TestImports_App/Header2.h> // Actual path
