00570c35334b17b84e3b07ec1cf728c0659c14d3
Jerry Marino <i@jerrymarino.com>
2021-09-30 12:57:35 -0700

    Fix LLDB internal module compilation error (#314)
    * Fix LLDB internal module compilation error
    
    When LLDB compiles swiftmodules during attachment and `po` it is missing
    some clang importer arguments. This adds missing copts directly to
    LLDB in the lldbinit.
    
    Note that I have also added a _example_ test case for LLDB and clarified some of
    the requirments here. For now, it atleast documents this requirment:
    ```
    Xcode project: bazel run tests/ios/xcodeproj:Test-Imports-App-Project
    && open tests/ios/xcodeproj/Test-Imports-App-Project.xcodeproj/
    Scheme: `TestImports-App`
    Breakpoints: tests/ios/unit-test/test-imports-app/empty.swift:6
    Breakpoints: tests/ios/unit-test/test-imports-app/empty.swift:15
    Xcode Version: 12.5.1
    ```
    
    Longer term, can "Reverse sherlock" some LLDB automation testing from
    swift-LLDB to verify debugging of many examples. Testing is further
    blocked by getting the debugger working on the examples. As of today,
    the examples just don't work on master
    https://github.com/bazel-ios/rules_ios/issues/281
    
    * Disable extra header path

--- tests/ios/unit-test/test-imports-app/Header.h
+++ tests/ios/unit-test/test-imports-app/Header.h
@@ -1,1 +1,5 @@
-#import <TestImports-App/Header2.h>
+// Note: rules_ios currently supports both of the names.
+// - Xcode target name
+// - "Module" name
+#import <TestImports-App/Header2.h> // Path from headermap
+// #import <TestImports_App/Header2.h> // Actual path
--- tests/ios/unit-test/test-imports-app/main.m
+++ tests/ios/unit-test/test-imports-app/main.m
@@ -1,5 +1,6 @@
 #import "Header.h"
 #import <TestImports-App/Header.h>
+#import <TestImports-App/TestImports_App-Swift.h>
 
 #ifdef __IPHONE_OS_VERSION_MIN_REQUIRED
 @import UIKit;
@@ -17,7 +18,8 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = [UIViewController new];
     self.window.rootViewController.view.backgroundColor = UIColor.whiteColor;
-
+    NSAssert([EmptyClass emptyDescription] != nil, @"Empty class description exists");
+    NSAssert([[EmptyClass new] emptyDescription] != nil, @"Empty instance description exists");
     [self.window makeKeyAndVisible];
 
     return YES;
