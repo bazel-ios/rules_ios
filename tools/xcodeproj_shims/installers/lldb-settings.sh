45ddf38390cdcd3c45b70d6233f8cbd1d4ba51d7
Elton Gao <yuanfeng@squareup.com>
2022-02-24 12:11:58 -0500

    Fix unable to append settings that has new line character in it (#415)
    * Fix unable to append settings that has new line character in it
    
    also address shellcheck linter warnings

--- tools/xcodeproj_shims/installers/lldb-settings.sh
+++ tools/xcodeproj_shims/installers/lldb-settings.sh
@@ -13,7 +13,7 @@ set -euo pipefail
 #
 # The platform settings one points the current working directory (cwd) to workspace root.
 # This is needed for features relying on lldb remote debugging, such as `oso_prefix_is_pwd`.
-cat > $BAZEL_LLDB_INIT_FILE <<-END
+cat > "$BAZEL_LLDB_INIT_FILE" <<-END
 platform settings -w "$BAZEL_WORKSPACE_ROOT/"
 
 settings set target.source-map ./ "$BAZEL_WORKSPACE_ROOT/"
@@ -21,8 +21,10 @@ settings set target.sdk-path $SDKROOT
 settings set target.swift-framework-search-paths $FRAMEWORK_SEARCH_PATHS
 END
 
-if [ ! -z ${BAZEL_ADDITIONAL_LLDB_SETTINGS+x} ]; then
-  echo $BAZEL_ADDITIONAL_LLDB_SETTINGS >> $BAZEL_LLDB_INIT_FILE
+if [ -n "${BAZEL_ADDITIONAL_LLDB_SETTINGS+x}" ]; then
+  cat >> "$BAZEL_LLDB_INIT_FILE" <<-END
+${BAZEL_ADDITIONAL_LLDB_SETTINGS}
+END
 fi
 
 # Holds all swift-extra-clang-flags
@@ -30,7 +32,7 @@ LLDB_SWIFT_EXTRA_CLANG_FLAGS=()
 # Append extra clang flags if set
 if [[ -n ${BAZEL_LLDB_SWIFT_EXTRA_CLANG_FLAGS:-} ]]
 then
-  LLDB_SWIFT_EXTRA_CLANG_FLAGS+=($BAZEL_LLDB_SWIFT_EXTRA_CLANG_FLAGS)
+  LLDB_SWIFT_EXTRA_CLANG_FLAGS+=("$BAZEL_LLDB_SWIFT_EXTRA_CLANG_FLAGS")
 fi
 # Pass all 'FRAMEWORK_SEARCH_PATHS' to lldb
 # so debugging swift files in mixed swift/objc
@@ -89,7 +91,7 @@ fi
 # is not empty
 if [[ ${#LLDB_SWIFT_EXTRA_CLANG_FLAGS[@]} -ne 0 ]]
 then
-  cat >> $BAZEL_LLDB_INIT_FILE <<-END
+  cat >> "$BAZEL_LLDB_INIT_FILE" <<-END
 settings set -- target.swift-extra-clang-flags ${LLDB_SWIFT_EXTRA_CLANG_FLAGS[@]}
 END
 fi
@@ -97,7 +99,7 @@ fi
 BAZEL_EXTERNAL_DIRNAME="$BAZEL_WORKSPACE_ROOT/bazel-$(basename "$BAZEL_WORKSPACE_ROOT")/external"
 if [ -d "$BAZEL_EXTERNAL_DIRNAME" ]
 then
-  cat >> $BAZEL_LLDB_INIT_FILE <<-END
+  cat >> "$BAZEL_LLDB_INIT_FILE" <<-END
 settings append target.source-map ./external/ "$BAZEL_EXTERNAL_DIRNAME"
 END
 fi
