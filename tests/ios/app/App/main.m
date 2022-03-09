4c2cba50f904a114e778261baefd78a9bd5322b8
Jerry Marino <i@jerrymarino.com>
2022-03-09 14:53:12 -0800

    Revert ad-hoc file

--- tests/ios/app/App/main.m
+++ tests/ios/app/App/main.m
@@ -1,8 +1,6 @@
 @import FW;
 @import OnlySources;
-@import Foundation;
+
 int main(int argc, char **argv) {
-    NSString *x = @"some";
-    NSLog(@"X", x);
     return UIApplicationMain(argc, argv, nil, nil);
 }
