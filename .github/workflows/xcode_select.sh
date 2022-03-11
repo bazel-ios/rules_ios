09d751ba3fc87740f59ca4cc86c0fd78b34d7566
Jerry Marino <i@jerrymarino.com>
2022-03-10 20:44:40 -0800

    Add test flake for github actions

--- .github/workflows/xcode_select.sh
+++ .github/workflows/xcode_select.sh
@@ -10,11 +10,12 @@ sudo xcode-select -s /Applications/Xcode_12.5.1.app
 echo "Generating bazelrc"
 
 # Setup for github actions - these are very flaky
-echo "test --test_timeout=600" > user.bazelrc
-echo "build --jobs=6" >> user.bazelrc
+echo "test --test_timeout=600 --flaky_test_attempts=//tests/ios/...@3" > user.bazelrc
+echo "test --local_test_jobs=1" >> user.bazelrc
+
+echo "build --jobs=4" >> user.bazelrc
 echo "build --spawn_strategy=standalone" >> user.bazelrc
 echo "build --xcode_version_config=//:host_xcodes" >> user.bazelrc
-echo "test --local_test_jobs=1" >> user.bazelrc
 
 # `deleted_packages` is needed below in order to override the value of the .bazelrc file
 echo "build:ios --apple_platform_type=ios --deleted_packages=''" >> user.bazelrc
