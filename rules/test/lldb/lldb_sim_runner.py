0a9297889849576996d243ce4035906451a75b75
Jonathan Dierksen <1507580+dierksen@users.noreply.github.com>
2022-10-21 10:24:23 -0500

    gradually trying to fix tests

--- rules/test/lldb/lldb_sim_runner.py
+++ rules/test/lldb/lldb_sim_runner.py
@@ -8,22 +8,21 @@ import logging
 import time
 import threading
 import traceback
-import tempfile
-import shutil
-import json
 import contextlib
 
 logging.basicConfig(
     format="%(asctime)s.%(msecs)03d %(levelname)s %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
-    level=logging.INFO)
+    level=logging.INFO,
+)
 logger = logging.getLogger(__name__)
 
 
 def find_pid(app_name, udid):
-    """ Find the PID for the app and the udid of the simulator"""
+    """Find the PID for the app and the udid of the simulator"""
     system_processes = subprocess.Popen(
-        ['ps', '-aU', '0'], stdout=subprocess.PIPE).communicate()[0]
+        ["ps", "-aU", "0"], stdout=subprocess.PIPE
+    ).communicate()[0]
     out_pid = None
     needle = app_name + ".app"
     for misc_p in system_processes.decode("utf8").split("\n"):
@@ -46,9 +45,7 @@ def boot_simulator(simctl_path, udid):
     problems
     """
     logger.info("Launching simulator with udid: %s", udid)
-    subprocess.run(
-        ["xcrun", "simctl", "boot",  udid],
-        check=True)
+    subprocess.run(["xcrun", "simctl", "boot", udid], check=True)
     logger.debug("Simulator launched.")
     if not sim_template.wait_for_sim_to_boot(simctl_path, udid):
         raise Exception("Failed to launch simulator with UDID: " + udid)
@@ -68,13 +65,18 @@ def temporary_ios_simulator(ctx, simctl_path, device, version):
     """
     runtime_version_name = version.replace(".", "-")
     logger.info("Creating simulator, device=%s, version=%s", device, version)
-    simctl_create_result = subprocess.run([
-        simctl_path, "create", "TestDevice", device,
-        "com.apple.CoreSimulator.SimRuntime.iOS-" + runtime_version_name
-    ],
+    simctl_create_result = subprocess.run(
+        [
+            simctl_path,
+            "create",
+            "TestDevice",
+            device,
+            "com.apple.CoreSimulator.SimRuntime.iOS-" + runtime_version_name,
+        ],
         encoding="utf-8",
         check=True,
-        stdout=subprocess.PIPE)
+        stdout=subprocess.PIPE,
+    )
     udid = simctl_create_result.stdout.rstrip()
     ctx.SetStartedSimUDID(udid)
     yield udid
@@ -84,29 +86,45 @@ def pack_tree_artifact(ios_application_output_path, app_name):
     """This slows it down a bit to copy and zip up the app, make it handle a .app
     in another way
     """
-    archive_root = os.environ.get("TEST_UNDECLARED_OUTPUTS_DIR", os.path.join(os.path.dirname(
-        ios_application_output_path), "lldb-test-intermediate"))
+    archive_root = os.environ.get(
+        "TEST_UNDECLARED_OUTPUTS_DIR",
+        os.path.join(
+            os.path.dirname(ios_application_output_path), "lldb-test-intermediate"
+        ),
+    )
     archive_app_path = os.path.join(archive_root, "Payload")
     ipa_path = os.path.join(archive_root, app_name + ".ipa")
     subprocess.run(["mkdir", "-p", archive_app_path], check=True)
-    subprocess.run(["/usr/bin/rsync",
-                    "--archive",
-                    "--delete",
-                    "--checksum",
-                    "--chmod=u+w",
-                    os.path.realpath(ios_application_output_path),
-                    archive_app_path
-                    ], check=True)
-    subprocess.run(["zip", "-rq", ipa_path, "Payload", ],
-                   check=True, cwd=archive_root)
+    subprocess.run(
+        [
+            "/usr/bin/rsync",
+            "--archive",
+            "--delete",
+            "--checksum",
+            "--chmod=u+w",
+            os.path.realpath(ios_application_output_path),
+            archive_app_path,
+        ],
+        check=True,
+    )
+    subprocess.run(
+        [
+            "zip",
+            "-rq",
+            ipa_path,
+            "Payload",
+        ],
+        check=True,
+        cwd=archive_root,
+    )
 
     return ipa_path
 
 
-def run_app_in_simulator(ctx, simctl_path, simulator_udid,
-                         ios_application_output_path, app_name):
-    """Installs and runs an app in the specified simulator.
-    """
+def run_app_in_simulator(
+    ctx, simctl_path, simulator_udid, ios_application_output_path, app_name
+):
+    """Installs and runs an app in the specified simulator."""
     logger.info("Booting simulator")
     try:
         boot_simulator(simctl_path, simulator_udid)
@@ -114,31 +132,40 @@ def run_app_in_simulator(ctx, simctl_path, simulator_udid,
         logger.info("Second attempt to boot simulator")
         boot_simulator(simctl_path, simulator_udid)
 
-    with sim_template.extracted_app(pack_tree_artifact(ios_application_output_path, app_name), app_name) as app_path:
-        logger.debug("Installing app %s to simulator %s",
-                     app_path, simulator_udid)
-        subprocess.run([simctl_path, "install", simulator_udid, app_path],
-                       check=True)
+    with sim_template.extracted_app(
+        pack_tree_artifact(ios_application_output_path, app_name), app_name
+    ) as app_path:
+        logger.debug("Installing app %s to simulator %s", app_path, simulator_udid)
+        subprocess.run([simctl_path, "install", simulator_udid, app_path], check=True)
         app_bundle_id = sim_template.bundle_id(app_path)
-        logger.info("Launching app %s in simulator %s", app_bundle_id,
-                    simulator_udid)
+        logger.info("Launching app %s in simulator %s", app_bundle_id, simulator_udid)
         args = [
-            simctl_path, "launch", "--wait-for-debugger", simulator_udid, app_bundle_id
+            simctl_path,
+            "launch",
+            "--wait-for-debugger",
+            simulator_udid,
+            app_bundle_id,
         ]
 
         # This returns the pid of the process
-        simctl_process = subprocess.Popen(args, env=sim_template.simctl_launch_environ(
-        ),  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        simctl_process = subprocess.Popen(
+            args,
+            env=sim_template.simctl_launch_environ(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
         sim_pid = simctl_process.pid
 
         # Stream the simctl output to stdout, consider moving to a file
         reader_tstdout = threading.Thread(
-            target=monitor_output, args=(simctl_process.stdout, "simctl"))
+            target=monitor_output, args=(simctl_process.stdout, "simctl")
+        )
         reader_tstdout.daemon = True
         reader_tstdout.start()
 
         reader_tstderr = threading.Thread(
-            target=monitor_output, args=(simctl_process.stderr, "simctl"))
+            target=monitor_output, args=(simctl_process.stderr, "simctl")
+        )
         reader_tstderr.daemon = True
         reader_tstderr.start()
 
@@ -162,15 +189,18 @@ def run_app_in_simulator(ctx, simctl_path, simulator_udid,
             ctx.Fail()
 
 
-def sim_thread_main(ctx, sim_device, sim_os_version, ios_application_output_path, app_name,
-                    minimum_os):
-    with temporary_ios_simulator(ctx, ctx.simctl_path, sim_device,
-                                 sim_os_version) as simulator_udid:
-        run_app_in_simulator(ctx, ctx.simctl_path, simulator_udid,
-                             ios_application_output_path, app_name)
+def sim_thread_main(
+    ctx, sim_device, sim_os_version, ios_application_output_path, app_name, minimum_os
+):
+    with temporary_ios_simulator(
+        ctx, ctx.simctl_path, sim_device, sim_os_version
+    ) as simulator_udid:
+        run_app_in_simulator(
+            ctx, ctx.simctl_path, simulator_udid, ios_application_output_path, app_name
+        )
 
 
-class TestContext():
+class TestContext:
     def __init__(self, pid, app_name, test_root):
         self.app_name = app_name
         self.test_root = test_root
@@ -181,18 +211,24 @@ class TestContext():
 
         # Bazel sends the MacOS SDK for py interpreter
         if "SDKROOT" in os.environ:
-            self.developer_dir = os.path.dirname(os.path.dirname(
-                os.path.dirname(os.path.dirname(os.path.dirname(os.environ["SDKROOT"])))))
+            self.developer_dir = os.path.dirname(
+                os.path.dirname(
+                    os.path.dirname(
+                        os.path.dirname(os.path.dirname(os.environ["SDKROOT"]))
+                    )
+                )
+            )
         else:
-            xcode_select_result = subprocess.run(["xcode-select", "-p"],
-                                                 encoding="utf-8",
-                                                 check=True,
-                                                 stdout=subprocess.PIPE)
+            xcode_select_result = subprocess.run(
+                ["xcode-select", "-p"],
+                encoding="utf-8",
+                check=True,
+                stdout=subprocess.PIPE,
+            )
 
             self.developer_dir = xcode_select_result.stdout.rstrip()
         os.environ["DEVELOPER_DIR"] = self.developer_dir
-        self.simctl_path = os.path.join(
-            self.developer_dir, "usr", "bin", "simctl")
+        self.simctl_path = os.path.join(self.developer_dir, "usr", "bin", "simctl")
 
     def SetStartedAppPid(self, app_pid):
         self.pid = app_pid
@@ -209,8 +245,7 @@ class TestContext():
     def Fail(self):
         traceback.print_exc()
         logger.error("FAIL")
-        with ctxlock:
-            self.status = -1
+        self.status = -1
 
     # Returns None for in progress, -1 fail, 1 pass
     def GetCompletionStatus(self):
@@ -228,7 +263,7 @@ class TestContext():
 def monitor_output(out, prefix, dupe_file_path=None):
     # Copy these lines to a file
     dupe_file = open(dupe_file_path, "w") if dupe_file_path else None
-    for line in iter(out.readline, b''):
+    for line in iter(out.readline, b""):
         str_line = line.decode("utf8")
         logger.info(prefix + " " + str_line.rstrip("\n"))
         if dupe_file:
@@ -245,19 +280,30 @@ def attach_debugger(ctx, test_root, pid, lldbinit, stdout):
     args = ["xcrun", "lldb", "-p", str(pid)]
     lldb_desc = " ".join(args)
     src_str = "command source " + str(lldbinit) + "\n"
-    logger.info("spawning LLDB with args: cd %s && %s - init %s",
-                test_root, lldb_desc + " - " + src_str, lldbinit)
+    logger.info(
+        "spawning LLDB with args: cd %s && %s - init %s",
+        test_root,
+        lldb_desc + " - " + src_str,
+        lldbinit,
+    )
     lldb_process = subprocess.Popen(
-        args, cwd=test_root, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        args,
+        cwd=test_root,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
 
     # Stream the LLDB output to stdout, consider moving to a file
     reader_t = threading.Thread(
-        target=monitor_output, args=(lldb_process.stdout, "LLDB", stdout))
+        target=monitor_output, args=(lldb_process.stdout, "LLDB", stdout)
+    )
     reader_t.daemon = True
     reader_t.start()
 
     reader_tstderr = threading.Thread(
-        target=monitor_output, args=(lldb_process.stderr, "LLDB"))
+        target=monitor_output, args=(lldb_process.stderr, "LLDB")
+    )
     reader_tstderr.daemon = True
     reader_tstderr.start()
 
@@ -265,8 +311,7 @@ def attach_debugger(ctx, test_root, pid, lldbinit, stdout):
         ctx.Fail()
 
     # Source the test lldbinit - ideally this can fail faster if it breaks
-    lldb_process.stdin.write(
-        ("command source " + str(lldbinit) + "\n").encode('utf-8'))
+    lldb_process.stdin.write(("command source " + str(lldbinit) + "\n").encode("utf-8"))
     lldb_process.stdin.flush()
 
     while lldb_process.poll() == None:
@@ -280,19 +325,19 @@ def attach_debugger(ctx, test_root, pid, lldbinit, stdout):
 def cleanup(ctx):
     udid = ctx.udid
 
-    subprocess.run([ctx.simctl_path, "shutdown", udid],
-                   stderr=subprocess.DEVNULL,
-                   check=False)
+    subprocess.run(
+        [ctx.simctl_path, "shutdown", udid], stderr=subprocess.DEVNULL, check=False
+    )
     logger.info("Deleting simulator with udid: %s", udid)
     subprocess.run([ctx.simctl_path, "delete", udid], check=True)
 
 
 def run_lldb(app_path, sdk, device, lldbinit_path, test_root, lldb_stdout):
-    """ Spawns a simulator with the `app_path`, `sdk`, device wit the `.lldbinit`
+    """Spawns a simulator with the `app_path`, `sdk`, device wit the `.lldbinit`
 
-        It waits for LLDB to exit and retuns the exit code
+    It waits for LLDB to exit and retuns the exit code
 
-        stdout is written to lldb.stdout in the test_root
+    stdout is written to lldb.stdout in the test_root
     """
     if not os.path.exists(app_path) or not app_path.endswith(".app"):
         raise Exception(f"Missing .app / [ --app ] %s", app_path)
@@ -314,8 +359,13 @@ def run_lldb(app_path, sdk, device, lldbinit_path, test_root, lldb_stdout):
     try:
         ctx = TestContext(None, app_name, str(test_root))
         sim_thread_main(ctx, device, sdk, app_path, ctx.app_name, sdk)
-        attach_debugger(ctx, test_root=ctx.GetTestRoot(),
-                        pid=ctx.GetStartedAppPid(), lldbinit=lldbinit_path, stdout=lldb_stdout)
+        attach_debugger(
+            ctx,
+            test_root=ctx.GetTestRoot(),
+            pid=ctx.GetStartedAppPid(),
+            lldbinit=lldbinit_path,
+            stdout=lldb_stdout,
+        )
 
         exit_code = ctx.GetCompletionStatus()
     except Exception:
