7a12c10542c19f45e8f79547ceb659fd0a1bd507
Derek Ostrander (derko) <derko@squareup.com>
2022-10-25 10:21:19 -0400

    Add ability to split a test bundle. (#545)
    This new macro allows for you to specify a way to "split" an identical bundle into multiple rules with different args for each. To do this you'd specify the different arguments you want to in each split. The suffix of each split will be specified by the key in the dictionary, the different args will be the value. These args will be merged with the ios_test_kwargs specified.
    
    For instance if we have:
    `split_ios_test(name = "Foo", split_name_to_kwargs {"MyTestHost1": {"test_host" : //MyTestHost1}, "MyTestHost2": {"test_host": //MyTestHost2}})`
    
    Then target //Foo would have 2 test rules created. 1 named `//Foo_MyTestHost1` and `//Foo_MyTestHost2` and will be packaged together in a test suite of `//Foo`.
    
    Also update rules_apple to 5.x head

---
+++ tests/test-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+## split tests
+test_suite_count=$(bazelisk query 'kind("test_suite", //tests/ios/unit-test:SplitTests)' | wc -l | xargs)
+if [[ "$test_suite_count" != "1" ]]; then
+  echo "Expected 1 test suite for split tests got $test_suite_count"
+  exit 1
+fi
+
+tests_count=$(bazelisk query 'tests(kind("test_suite", //tests/ios/unit-test:SplitTests))' | wc -l | xargs)
+if [[ "$tests_count" != "2" ]]; then
+  echo "More than 2 tests for split tests got $tests_count"
+  exit 1
+fi
