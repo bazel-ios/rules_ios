d3267dc2570f23e4fee15fae84568e073f2902e0
Matt Robinson <matt.robinson@reddit.com>
2023-01-17 14:09:49 -0700

    Match `com.apple.product-type.bundle.unit-test` input options

--- tools/xcodeproj_shims/install.sh
+++ tools/xcodeproj_shims/install.sh
@@ -56,7 +56,7 @@ com.apple.product-type.app-extension)
 com.apple.product-type.bundle.ui-testing)
     input_options=(
         "bazel-bin/$BAZEL_BIN_SUBDIR/${FULL_PRODUCT_NAME}"
-        "bazel-bin/$BAZEL_BIN_SUBDIR/${TARGET_NAME}_archive-root/Payload/$TARGET_NAME${WRAPPER_SUFFIX:-}"
+        "bazel-bin/$BAZEL_BIN_SUBDIR/$TARGET_NAME.__internal__.__test_bundle_archive-root/$TARGET_NAME${WRAPPER_SUFFIX:-}"
         "bazel-bin/$BAZEL_BIN_SUBDIR/${BAZEL_BUILD_TARGET_LABEL#*:}.runfiles/${BAZEL_BUILD_TARGET_WORKSPACE}/${BAZEL_BIN_SUBDIR}/${FULL_PRODUCT_NAME}"
         "bazel-bin/$BAZEL_BIN_SUBDIR/${BAZEL_BUILD_TARGET_LABEL#*:}.runfiles/${BAZEL_BUILD_TARGET_WORKSPACE}/${BAZEL_BIN_SUBDIR}/$TARGET_NAME.zip"
     )
