6def2640c9d7a6f9da9acb2b253e67485fc9b774
Jerry Marino <i@jerrymarino.com>
2023-02-24 18:30:10 -0800

    Automatically recurse into the login shell

--- tests/xcodeproj-tests.sh
+++ tests/xcodeproj-tests.sh
@@ -81,8 +81,15 @@ update() {
 }
 
 if [[ "$(arch)" == "arm"* ]]; then
-    echo -e "error: rerun where Bazel is an x64_64 bazel:\narch -arch x86_64 /bin/bash -l -c \"$0 ${@}\""
-    exit 1
+    echo -e "warning: rerun where Bazel is an x64_64 bazel:\narch -arch x86_64 /bin/bash -l -c \"$0 ${@}\""
+
+    # This should work - rules_ios has been proven to work in this. If you
+    # don't have the right version of Bazelisk then install it.
+    #
+    # This is tested on bash Montery M1 Max to work. A lot of these tools will
+    # not work when spawned under rosetta without a login shell
+    arch -arch x86_64 $SHELL -l -c "$0 ${@}"
+    exit $?
 fi
 
 
