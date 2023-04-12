ed0cb3173bf8f87367badcdc9d818bfa25113e80
i@jerrymarino.com <i@jerrymarino.com>
2023-04-12 16:57:50 -0700

    Updating ios_unit_snapshot_test

--- rules/test/snapshot/BazelRunfileBundleBridge.mm
+++ rules/test/snapshot/BazelRunfileBundleBridge.mm
@@ -1,6 +1,11 @@
 #import "BazelRunfileBundleBridge.h"
+#include "tools/cpp/runfiles/runfiles.h"
+#include <string>
 
 @implementation NSBundle (BazelRunfileBundleBridge)
+
+using namespace bazel::tools::cpp::runfiles;
+
 
 - (NSString *)bazelRunfilePathForResource:(NSString *)resource
 {
@@ -9,6 +14,9 @@
     // e.g.
     // Does the existing C++ code load it correctly, otherwise what should we
     // do?
+    std::string error;
+    auto runfiles = Runfiles::CreateForTest(&error);
+    NSLog(@"Creating runfiles: %s", runfiles.c_str());
     return nil;
 }
 
