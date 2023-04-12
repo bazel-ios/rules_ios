a1a5d80debd6516febc2182647d66c665372e63a
steve-sun-me <117430445+steve-sun-me@users.noreply.github.com>
2023-04-12 13:08:20 -0700

    [DNM] Implement runfiles in BundleBridge

--- rules/test/snapshot/BundleBridge.mm
+++ rules/test/snapshot/BundleBridge.mm
@@ -1,14 +1,29 @@
 #import "BundleBridge.h"
+#include "tools/cpp/runfiles/runfiles.h"
+#include <string>
+
+using namespace bazel::tools::cpp::runfiles;
 
 @implementation NSBundle (BazelBundleBridge)
 
 - (NSString *)bazelRunfilePathForResource:(NSString *)resource
 {
-    // TODO Determine:
-    // Does the existing C++ code solving our problem, if so that is awesome and
-    // potentially  more robust than a simple implementation that we do for
-    // rules_ios which achieves the same result
-    return nil;
+    std::string error;
+
+    auto runfiles = Runfiles::CreateForTest(&error);
+
+    if (!runfiles) {
+        NSLog(@"Error creating runfiles: %s", error.c_str());
+        return nil;
+    }
+
+    std::string resource_path = runfiles->Rlocation(resource.UTF8String);
+    if (resource_path.empty()) {
+        NSLog(@"Resource not found: %@", resource);
+        return nil;
+    }
+
+    return [NSString stringWithUTF8String:resource_path.c_str()];
 }
 
 @end
