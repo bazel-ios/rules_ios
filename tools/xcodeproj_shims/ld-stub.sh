7d16d98b2ec84de09b3e7e39df328bfa8f3a0b26
Thiago Cruz <thiago.hmcruz@gmail.com>
2023-10-25 16:47:01 -0400

    [Xcode 15] Fix libtool version check (#791)

--- tools/xcodeproj_shims/ld-stub.sh
+++ tools/xcodeproj_shims/ld-stub.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -eu
 
+original_args=("$@")
+
 while :; do
     test $# -eq 0 && exit 0
     case $1 in
+        -V)
+            # Xcode 15+ needs to know the version of libtool
+            exec libtool "${original_args[@]}"
+            ;;
         *.dat)
             # provides the header for an empty .dat file
             echo -n -e '\x00lld\0' > $1
