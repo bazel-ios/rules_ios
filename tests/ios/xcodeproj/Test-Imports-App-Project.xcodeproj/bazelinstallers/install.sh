172891dc388b1aee6824265955b94c585a625e51
Luis Padron <lpadron@squareup.com>
2024-03-01 10:10:28 -0500

    Update installer.sh for dynaminc framework path changes

--- tests/ios/xcodeproj/Test-Imports-App-Project.xcodeproj/bazelinstallers/install.sh
+++ tests/ios/xcodeproj/Test-Imports-App-Project.xcodeproj/bazelinstallers/install.sh
@@ -10,9 +10,9 @@ find "${BAZEL_DIAGNOSTICS_DIR}" -type f -atime +7d -delete
 
 case "${PRODUCT_TYPE}" in
 com.apple.product-type.framework)
-    input_options=("bazel-bin/$BAZEL_BIN_SUBDIR/${TARGET_NAME}/${FULL_PRODUCT_NAME}")
+    input_options=("bazel-bin/$BAZEL_BIN_SUBDIR/${TARGET_NAME}.framework")
     if [ ! -d "$input_options" ]; then
-        input_options=("bazel-bin/$BAZEL_BIN_SUBDIR/${FULL_PRODUCT_NAME}")
+        input_options=("bazel-bin/$BAZEL_BIN_SUBDIR/${TARGET_NAME}.framework.zip")
     fi
     ;;
 com.apple.product-type.framework.static)
