2bfac3dd96474f91939bae3424b1d134da18f6e3
Luis Padron <lpadron@squareup.com>
2024-06-18 12:20:42 -0400

    Fix extension_safe attr not passing copts
    When using `extension_safe` the `copt` for the objc/swift library must also supply `-fapplication-extension`/`-application-extension`

--- tests/ios/frameworks/dynamic/b/lib.swift
+++ tests/ios/frameworks/dynamic/b/lib.swift
@@ -1,6 +1,16 @@
 import c
+import WidgetKit
 
 public struct B {
     public static func run() { C.run() }
     public static func NATURE_OF_B() { B.run() }
+
+    @available(iOS 14.0, *)
+    public static func runExtension() -> [WidgetFamily] {
+        if #available(iOSApplicationExtension 16.0, *) {
+            [.accessoryCircular]
+        } else {
+            []
+        }
+    }
 }
