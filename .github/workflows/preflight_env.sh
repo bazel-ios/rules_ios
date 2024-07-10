1be32c0165304a22b63e91739ad5af51bf2005e6
Luis Padron <lpadron@squareup.com>
2024-07-10 15:11:50 -0400

    Add CI tests for Build without the Bytes
    Adds CI tests for Build without the Bytes and new rules_swift 2.x

--- .github/workflows/preflight_env.sh
+++ .github/workflows/preflight_env.sh
@@ -15,6 +15,14 @@ if [[ "$*" == *--no-bzlmod* ]]; then
   echo "build --noenable_bzlmod" >> user.bazelrc
 fi
 
+# If `RULES_APPLE_SWIFT_VERSION` is set to `latest` use the master branch of rules_swift and rules_apple.
+if [[ "${RULES_APPLE_RULES_SWIFT_VERSION}" == "latest" ]]; then
+  gh repo clone bazelbuild/rules_swift /tmp/rules_swift
+  gh repo clone bazelbuild/rules_apple /tmp/rules_apple
+  echo "common --override_module=rules_swift=/tmp/rules_swift" >> user.bazelrc
+  echo "common --override_module=rules_apple=/tmp/rules_apple" >> user.bazelrc
+fi
+
 # Add the ci config override.
 echo "common --config=ci" >> user.bazelrc
 
