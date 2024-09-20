fde4de5b2e121542fa16d6326f55e965f9a29d2d
xujiguang.kuper <xujiguang.kuper@bytedance.com>
2024-09-20 16:46:57 +0800

    fix MobileFlow deps MiSnapCore issue

---
+++ tests/ios/in-tree-vendor-prebuilt-deps/Pods/MiSnap/Frameworks/MiSnapCore.framework/Headers/MiSnapCore.h
@@ -0,0 +1,18 @@
+//
+//  MiSnapCore.h
+//  MiSnapCore
+//
+//  Created by Stas Tsuprenko on 12/30/22.
+//
+
+#import <Foundation/Foundation.h>
+/**
+ Umbrella header
+ */
+@interface MiSnapCore : NSObject
+/**
+ Version
+ */
++ (NSString * _Nonnull)version;
+
+@end
